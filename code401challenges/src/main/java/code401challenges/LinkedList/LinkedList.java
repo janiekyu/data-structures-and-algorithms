88632524e45f0253dadf0c7c33bced16b8388c02
Jane Hur <janehur8@gmail.com>
2019-08-12 23:21:03 -0700

    code challenge 6 complete

--- code401challenges/src/main/java/code401challenges/LinkedList/LinkedList.java
+++ code401challenges/src/main/java/code401challenges/LinkedList/LinkedList.java
@@ -44,6 +44,62 @@ public class LinkedList<T> {
         return values;
     }
 
+    public void append(T value) {
+        Node<T> current = head;
+        Node<T> previous = null;
+        while (current != null){
+            previous = current;
+            current = current.nextNode;
+        }
+        Node<T> nodeToAppend = new Node<> (value, previous.nextNode);
+        previous.nextNode = nodeToAppend;
+    }
+
+    public void insertBefore(T valueToFind, T valueToAdd){
+
+        // if the node that holds the given value is in the first node,
+        // call the insert() method and exit the method
+        if (head.currentNodeValue == valueToFind){
+            insert(valueToAdd);
+            return;
+        }
+
+        Node<T> current = head;
+        Node<T> previous = new Node<>(null, null);
+
+        while (current.currentNodeValue != valueToFind){
+            if (current.nextNode == null){
+                // If the value is not in linked list exit the method
+                System.out.println("Value not in list");
+                return;
+            } else {
+                previous = current;
+                current = current.nextNode;
+            }
+        }
+
+        Node<T> nodeToAppend = new Node<> (valueToAdd, previous.nextNode);
+        previous.nextNode = nodeToAppend;
+    }
+
+    public void insertAfter (T valueToFind, T valueToAdd){
+        Node current = head;
+        Node next = head.nextNode;
+
+        while (current != null) {
+            //System.out.println("current: " + current.currentNodeValue);
+            if (current.currentNodeValue == valueToFind) {
+                //System.out.println("Found value");
+                Node<T> newNode = new Node<>(valueToAdd, current.nextNode);
+                current.nextNode = newNode;
+                return;
+            } else {
+                //System.out.println("Still Looking");
+                current = current.nextNode;
+            }
+        }
+    }
+
 }
 
 
---
+++ otherReadmes/linkedLists-codeChallenge.md
@@ -0,0 +1,21 @@
+# Code Challenge 06: Linked list insertions
+
+## Challenge
+Write the following methods for the Linked List class:
+- `.append(value)` which adds a new node with the given value to the end of the list
+- `.insertBefore(value, newVal)` which add a new node with the given `newValue` immediately before the first `value` node
+- `.insertAfter(value, newVal)` which add a new node with the given `newValue` immediately after the first `value` node
+
+## Approach & Efficiency
+- Walk through the linked list to find the location where the new node needs to be inserted
+- Create the new node with given value and set the next node appropriately
+- Set the next node properties of the nodes before and after the new node as needed
+
+## Solution
+[Code](https://github.com/janiekyu/data-structures-and-algorithms/tree/master/code401challenges/src/main/java/code401challenges/LinkedList) | [Test](https://github.com/janiekyu/data-structures-and-algorithms/blob/master/code401challenges/src/test/java/code401challenges/LinkedList/LinkedListTest.java)
+
+Whiteboarded with Promila Tamang :
+
+![Image of whiteboard](../assets/cc06-image-a.jpg)
+
+![Image of whiteboard](../assets/cc06-image-b.jpg)
